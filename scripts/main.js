dc750c467c9d89e29d23fccd6f2e5d0acb19e113
John Asper <agentx1994@gmail.com>
2016-11-14 11:10:19 -0500

    Adds animation, makes background image fixed
    Adds a fadeIn to teh main header h1, and slideDown's to the #main>div
    sections.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 
 $(document).ready(function () {
-	
+ 	$('#main-header').hide().fadeIn('slow');
+ 	$('#projects').hide();
+ 	$('#education,#interests').hide().slideDown('slow', function () {
+ 		$('#projects').slideDown('slow');
+ 	});
 })
